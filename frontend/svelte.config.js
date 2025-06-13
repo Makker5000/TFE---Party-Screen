33edf62d433ac2c86574f8a2e1f3f650a3e9cf66
Makker5000 <brudetilleux@gmail.com>
2025-06-13 20:26:06 +0200

    Update : BugFix 'svelte-preprocess'

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -12,11 +12,11 @@
 
 // frontend/svelte.config.js
 import vercel from '@sveltejs/adapter-vercel';
-import preprocess from 'svelte-preprocess';
+// import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
+//   preprocess: preprocess(),
 
   kit: {
     adapter: vercel(),
