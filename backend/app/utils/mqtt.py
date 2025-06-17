85cb2ee7519665b983dfd6328ee268850e77b6b6
Makker5000 <brudetilleux@gmail.com>
2025-06-18 01:44:09 +0200

    MQTTS : Update : One client amorced

--- backend/app/utils/mqtt.py
+++ backend/app/utils/mqtt.py
@@ -24,18 +24,18 @@ from app.core.config import MQTT_BROKER, MQTT_PORT, MQTT_TOPIC, CA_CERT_PATH, CL
 
 client = mqtt.Client()
 
-def connect_mqtt():
-    # Chargement du certificat CA pour vérifier le broker
-    client.tls_set(
-        ca_certs=CA_CERT_PATH,
-        certfile=CLIENT_CERT,       # ou "path/to/client.crt" si mutual TLS
-        keyfile=CLIENT_KEY,        # ou "path/to/client.key"
-        tls_version=ssl.PROTOCOL_TLSv1_2,
-        ciphers=None
-    )
-    # Si votre broker a un certificat self-signed et que vous êtes en dev :
-    client.tls_insecure_set(True)
+# Chargement du certificat CA pour vérifier le broker
+client.tls_set(
+    ca_certs=CA_CERT_PATH,
+    certfile=CLIENT_CERT,       # ou "path/to/client.crt" si mutual TLS
+    keyfile=CLIENT_KEY,        # ou "path/to/client.key"
+    tls_version=ssl.PROTOCOL_TLSv1_2,
+    ciphers=None
+)
+# Si votre broker a un certificat self-signed et que vous êtes en dev :
+client.tls_insecure_set(True)
 
+def connect_mqtt():
     try:
         client.connect(MQTT_BROKER, MQTT_PORT, 60)
     except Exception as e:
