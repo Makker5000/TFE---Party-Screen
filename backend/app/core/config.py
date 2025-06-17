ee3f3b9253a191d836c5ed37d2b40e1626d64910
Makker5000 <brudetilleux@gmail.com>
2025-06-18 01:10:52 +0200

    MQTTS : Setup Config and Certs

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,4 +1,7 @@
-MQTT_BROKER = "127.0.0.1"
-MQTT_PORT = 1883
+MQTT_BROKER = "mon.broker.mqtt"
+MQTT_PORT = 8883
 # MQTT_TOPIC = "topic/esp8266"  # utilisé si pas de topic dynamique
 MQTT_TOPIC = "esp/pere/commande"  # utilisé si pas de topic dynamique
+CA_CERT_PATH    = "/etc/mosquitto/certs/ca.crt"
+CLIENT_CERT     = "/etc/mosquitto/certs/CA_backend_client/backend-client.crt"
+CLIENT_KEY      = "/etc/mosquitto/certs/CA_backend_client/backend-client.key"
