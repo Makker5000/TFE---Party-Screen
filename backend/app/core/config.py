819d3fd28849d5c5809cb3d4a37a8d0b441c76dd
Makker5000 <brudetilleux@gmail.com>
2025-06-18 11:53:13 +0200

    MQTT : Update : port

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,7 @@
 # MQTT_BROKER = "mon.broker.mqtt"
 MQTT_BROKER = "192.168.1.10"
-MQTT_PORT = 8883
+# MQTT_PORT = 8883
+MQTT_PORT = 1883
 # MQTT_TOPIC = "topic/esp8266"  # utilisé si pas de topic dynamique
 MQTT_TOPIC = "esp/pere/commande"  # utilisé si pas de topic dynamique
 CA_CERT_PATH    = "/etc/mosquitto/certs/ca.crt"
